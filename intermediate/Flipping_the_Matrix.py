325aabf06e1456b44e6b0226ccccf44894e8a46a
aux-Issa <issa1116184028@gmail.com>
2022-05-07 23:36:50 +0900

    a bit of fix

--- intermediate/Flipping_the_Matrix.py
+++ intermediate/Flipping_the_Matrix.py
@@ -10,6 +10,6 @@ def flippingMatrix(matrix):
         for j in range(n//2):
             s += max(matrix[i][j], matrix[i][n-j-1], matrix[n-i-1][j], matrix[n-i-1][n-j-1])
     return s
-   
+
 flippingMatrix([[112, 42, 83, 119], [56, 125, 56, 49], [15, 78, 101, 43], [62, 98, 114, 108]])
-# expexted 414
+# expexted result = 414
