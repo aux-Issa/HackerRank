080cce1ecb9d0515d93abfba7a65d9883cd229e3
aux-Issa <issa1116184028@gmail.com>
2022-05-18 23:38:27 +0900

    fizz buzz

---
+++ etc/Flexible_FIzzBuzz.py
@@ -0,0 +1,19 @@
+# 10進数のn桁の数字が与えられる．この数字を入れ替えて最小の値を標準出力せよ
+# ただし先頭は0でないとする
+
+import random
+import pdb
+
+def main(lines):
+  for i, v in enumerate(lines):
+    print("line[{0}]:{1}".format(i,v))
+ 
+
+if __name__ == '__main__':
+  lines = []
+  for l in sys.stdin:
+    lines.ao
+
+num = random.randint(10**80, 10**100) 
+print(num)
+main(num)
