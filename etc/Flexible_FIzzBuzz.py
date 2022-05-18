50657a21eb170e7fcb5f1c34f292a31125a947b4
aux-Issa <issa1116184028@gmail.com>
2022-05-19 00:32:37 +0900

    flexible fizzbuzz

--- etc/Flexible_FIzzBuzz.py
+++ etc/Flexible_FIzzBuzz.py
@@ -1,19 +1,29 @@
 # 10進数のn桁の数字が与えられる．この数字を入れ替えて最小の値を標準出力せよ
 # ただし先頭は0でないとする
 
-import random
-import pdb
+import sys
 
 def main(lines):
-  for i, v in enumerate(lines):
-    print("line[{0}]:{1}".format(i,v))
- 
+  target = int(lines[-1])
+  # print(lines)
+  lines.pop()
+  lines = [l.split(':') for l in lines]
+  dic = {int(l[0]):l[1] for l in lines}
+  ans_key = []
+  for key, val in dic.items():
+    if target%key==0:
+      ans_key.append(key)
 
-if __name__ == '__main__':
-  lines = []
-  for l in sys.stdin:
-    lines.ao
+  ans_key.sort()
+  ans_words = [dic[key] for key in ans_key]
+  print(''.join(ans_words) if ans_words else target) 
 
-num = random.randint(10**80, 10**100) 
-print(num)
-main(num)
+
+lines = []
+for l in sys.stdin:
+  lines.append(l.rstrip('\r\n'))
+main(lines)
+
+# num = random.randint(10**80, 10**100) 
+# print(num)
+# main(num)
